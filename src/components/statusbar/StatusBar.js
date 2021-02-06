33d6093468e8bb6c8751c109f23046679691964b
prnachap <33637318+prnachap@users.noreply.github.com>
2021-02-06 18:28:15 +0530

    Update StatusBar.js

--- src/components/statusbar/StatusBar.js
+++ src/components/statusbar/StatusBar.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "./statusbar.scss";
 
-const StatusBar = ({ onComplete, onActive, onAll }) => {
+const StatusBar = ({ onComplete, onActive, onAll, type }) => {
   return (
     <div className="status-mobile container">
       <h4
-        className="status-mobile__text status-mobile__text--active  "
+        className={`status-mobile__text ${
+          type === "all" && "status-mobile__text--active"
+        }`}
         onClick={onAll}
       >
         All
       </h4>
-      <h4 className="status-mobile__text" onClick={onActive}>
+      <h4
+        className={`status-mobile__text ${
+          type === "active" && "status-mobile__text--active"
+        }`}
+        onClick={onActive}
+      >
         Active
       </h4>
-      <h4 className="status-mobile__text" onClick={onComplete}>
+      <h4
+        className={`status-mobile__text ${
+          type === "completed" && "status-mobile__text--active"
+        }`}
+      >
         Completed
       </h4>
     </div>
